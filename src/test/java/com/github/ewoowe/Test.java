09430b33e32492880883da57774f141fe1856c6e
ewoowe <1394166475@qq.com>
2023-04-18 21:49:47 +0800

    add test

--- src/test/java/com/github/ewoowe/Test.java
+++ src/test/java/com/github/ewoowe/Test.java
@@ -1,10 +1,31 @@
 package com.github.ewoowe;
 
+import java.util.function.Function;
+
 public class Test {
+
+    public static class IpSetter implements Function<String, String> {
+
+        private String ip;
+
+        private IpSetter(String ip) {
+            this.ip = ip;
+        }
+
+        @Override
+        public String apply(String s) {
+            String[] split = s.split("/");
+            if (split.length != 2) {
+                throw new IllegalArgumentException("ip incorrect");
+            }
+            return ip + "/" + split[1];
+        }
+    }
+
     public static void main(String[] args) {
-        Yummy.set("P:\\WorkSpace\\yummy\\src\\test\\resources\\test.yaml",
-                new String[]{"a.b[1].c", "a.b[0][1].d", "a.b[0].c", "a.c.d"},
-                new Object[]{"new1", "new2", "new3", "new4"},
-                false);
+        Yummy.set("test.yaml",
+                new String[]{"a.b[1].e", "a.b[0][1].d", "a.b[0].c", "a.c.d", "a.b[1].c"},
+                new Object[]{"new1", "new2", "new3", "new4", new IpSetter("192.168.39.200")},
+                true);
     }
 }
